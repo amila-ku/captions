41bc531b15aad26b40127898e94956e944243605
amila-ku <amila.kumaranayaka@gmail.com>
2024-06-02 00:37:12 +0100

    Added caption backend and fixed display

--- backend/caption/caption.go
+++ backend/caption/caption.go
@@ -1,10 +1,68 @@
 package caption
 
-type Message struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	// "encore.dev/app/env"
+	// "encore.dev/beta/errs"
+	// "encore.dev/rfy"
+	openai "github.com/sashabaranov/go-openai" 
+)
+
+type CaptionRequestParams struct {
+	Location string `json:"location" validate:"required,max=20"`
+	Mood     string `json:"mood" validate:"required,max=20"`
+	Words    string `json:"words"`
 }
 
+// type CaptionRequestParams struct {
+//     Location string // location
+//     Mood string // number of blog posts to skip, for pagination
+// 	Words string
+// }
+
 type CaptionResponse struct {
-	Message Message `json:"message"`
+	Caption string `json:"caption"`
 }
+
+//encore:api public method=GET path=/captions
+func captionProcessor(ctx context.Context, opts CaptionRequestParams, ) (*CaptionResponse, error) {
+	openaiAPIKey := os.Getenv("OPENAI_API_KEY")
+	if openaiAPIKey == "" {
+		log.Fatalln("OPENAI_API_KEY environment variable not set")
+	}
+
+	client := openai.NewClient(openaiAPIKey)
+
+	resp, err := client.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model: openai.GPT3Dot5Turbo,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "You are an expert creative marketer. Create a caption for the location the user enters based on user's mood. Also include the words the user enters in the caption.",
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: fmt.Sprintf("Location: %s Mood: %s Words: %s", opts.Location, opts.Mood, opts.Words),
+			},
+		},
+	})
+	if err != nil {
+		log.Println("Error in creating campaigns from OpenAI:", err)
+		return nil, err
+	}
+
+	caption := resp.Choices[0].Message.Content
+	return &CaptionResponse{Caption: caption}, nil
+}
+
+// func init() {
+// 	app := rfy.New()
+// 	env.HandleFunc("/captions", func(ctx context.Context, req CaptionRequest) (*CaptionResponse, error) {
+// 		return captionProcessor(ctx, req)
+// 	})
+// 	env.Start(app)
+// }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module encore.app
 
 go 1.18
 
-require encore.dev v1.30.0 // indirect
+require (
+	encore.dev v1.30.0 // indirect
+	github.com/sashabaranov/go-openai v1.24.1 // indirect
+)
